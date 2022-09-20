09254bc5680ae041e448c755d8c0d808bfa5dc26
bertve <bert.vaneeckhoutte@student.hogent.be>
2022-09-20 16:20:28 +0200

    added mqtt publish support

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import RPi.GPIO as GPIO
 import signal
 import time
 import datetime
-import paho.mqtt.publish as publish
+import paho.mqtt.client as paho
+from paho import mqtt
 import json
 
 def end_run(signal, frame):
@@ -17,6 +18,54 @@ def countPulse(channel):
     global count
     if start_counter == 1:
         count = count + 1
+
+# MQTT
+hostname = "ac24c670632142bab0a422606038f608.s1.eu.hivemq.cloud"
+port = 8883
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+# enable TLS for secure connection
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("flexso", "hackflexso")
+# connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect(hostname, port)
+
+# setting callbacks, use separate functions like above for better visibility
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+# subscribe to all topics of encyclopedia by using the wildcard "#"
+#client.subscribe("encyclopedia/#", qos=1)
+
+# a single publish, this can also be done in loops, etc.
+#client.publish("/te", payload="hot", qos=1)
+
+# loop_forever for simplicity, here you need to stop the loop manually
+# you can also use loop_start and loop_stop
+#client.loop_forever()
 
 # ctrl + c => Stop
 signal.signal(signal.SIGINT, end_run)
@@ -37,25 +86,22 @@ GPIO.add_event_detect(FLOW_SENSOR_PIN, GPIO.FALLING, callback=countPulse)
 global count
 count = 0
 
-# MQTT
-hostname = "ac24c670632142bab0a422606038f608.s1.eu.hivemq.cloud"
-port = 8883
-
 # main loop
 keep_running = True
+
 while keep_running:
     try:
         start_counter = 1
         time.sleep(1)
         start_counter = 0
         flow = (count / 7.5)# Pulse freq (Hz) = 7.5Q, Q is flow rate in L/min
-        datetime = datetime.datetime.now()
-        message = {"flow": flow, "datetime": datetime.strftime("%Y-%m-%d %H:%M:%S"), "descr": "flow rate in L/min"}
+        dtime = datetime.datetime.now()
+        message = {"flow": flow, "datetime": dtime.strftime("%Y-%m-%d %H:%M:%S"), "descr": "flow rate in L/min"}
         msg_json = json.dumps(message)
         print(msg_json)
         # publish flow here
-        publish.single("/flowMeter", msg_json, hostname=hostname, retain=True, port=port)
+        client.publish("/flowMeter", payload=msg_json, qos=1, retain=True)
         count = 0
-        time.sleep(5)
+        time.sleep(1)
     except KeyboardInterrupt:
         end_run()
